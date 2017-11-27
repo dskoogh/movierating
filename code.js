c87db0b40e90fed435fb86496b9da5264aae6370
Dafvid Skoogh <dafvid.skoogh@gmail.com>
2017-11-27 17:06:03 +0100

    Add first

---
+++ code.js
@@ -0,0 +1,120 @@
+var chooseFilm = 0;
+
+function renderMovie(movie, film){
+    document.querySelector("#image img").setAttribute("src", movie[film].image);
+    document.getElementById("movietitle").innerText = movie[film].title;
+    document.getElementById("prop").innerText = movie[film].description;
+            
+    document.querySelector("#actrs").innerHTML = "";
+    for (let i=0; i<movie[film].actors.length; i++){
+        let node = document.createElement("li");
+        node.innerText = movie[film].actors[i];
+        document.querySelector("#actrs").appendChild(node);
+    }
+};
+
+function changeStarRating(grade) {
+    // Create variables
+    $("#stars span").removeClass("filledStars");
+
+    /*var s1 = document.getElementById("star1");
+    var s2 = document.getElementById("star2");
+    var s3 = document.getElementById("star3");
+    var s4 = document.getElementById("star4");
+    var s5 = document.getElementById("star5");
+
+    // Remove filled stars
+    s1.classList.remove("filledStars");
+    s2.classList.remove("filledStars");
+    s3.classList.remove("filledStars");
+    s4.classList.remove("filledStars");
+    s5.classList.remove("filledStars");*/
+    
+    switch(grade) {
+        case 5: $("#star5").addClass("filledStars");
+        case 4: $("#star4").addClass("filledStars");
+        case 3: $("#star3").addClass("filledStars");
+        case 2: $("#star2").addClass("filledStars");
+        case 1: $("#star1").addClass("filledStars");
+
+        /*case 5: s5.classList.add("filledStars");
+        case 4: s4.classList.add("filledStars");
+        case 3: s3.classList.add("filledStars");
+        case 2: s2.classList.add("filledStars");
+        case 1: s1.classList.add("filledStars");*/
+        return;
+    }
+};
+
+function hoverStarRating(grade) {
+    // Create variables
+    var s1 = document.getElementById("star1");
+    var s2 = document.getElementById("star2");
+    var s3 = document.getElementById("star3");
+    var s4 = document.getElementById("star4");
+    var s5 = document.getElementById("star5");
+
+    // Remove filled stars
+    s1.classList.remove("hoverStar");
+    s2.classList.remove("hoverStar");
+    s3.classList.remove("hoverStar");
+    s4.classList.remove("hoverStar");
+    s5.classList.remove("hoverStar");
+    
+    switch(grade) {
+        case 5: s5.classList.add("hoverStar");
+        case 4: s4.classList.add("hoverStar");
+        case 3: s3.classList.add("hoverStar");
+        case 2: s2.classList.add("hoverStar");
+        case 1: s1.classList.add("hoverStar");
+        return;
+    }
+};
+
+function createMovieButtons(movies) {
+    var numOfMov = movies.length;
+    var buttonList = "";
+    for (let i=0; i<numOfMov; i++) {
+        buttonList += "<span id=\"f" + i + "\">Film " + (i+1) + "</span>";
+    }
+    document.getElementById("filmSelector").innerHTML = buttonList;
+};
+
+document.querySelector("#stars")
+    .addEventListener("click", function(e) {
+        let star = parseInt(e.target.id.substring(4));
+        if (!isNaN(star)) {
+            changeStarRating(star);
+        } else return;
+    }
+);
+
+document.querySelector("#stars")
+    .addEventListener("mouseover", function(e){
+    let star = parseInt(e.target.id.substring(4));
+    //console.log(star);
+    hoverStarRating(star);
+});
+
+document.querySelector("#stars")
+    .addEventListener("mouseout", function(){
+    var s1 = document.getElementById("star1").classList.remove("hoverStar");
+    var s2 = document.getElementById("star2").classList.remove("hoverStar");
+    var s3 = document.getElementById("star3").classList.remove("hoverStar");
+    var s4 = document.getElementById("star4").classList.remove("hoverStar");
+    var s5 = document.getElementById("star5").classList.remove("hoverStar");
+});
+
+document.querySelector("#filmSelector")
+    .addEventListener("click", function(e){
+        let button = parseInt(e.target.id.substring(1));
+        if (!isNaN(button)) {
+            renderMovie(movieData, button);
+        }
+    }
+);
+
+createMovieButtons(movieData);
+
+renderMovie(movieData, chooseFilm);
+
---
+++ data.js
@@ -0,0 +1,32 @@
+let movieData = [
+    {
+        image: "http://www.spelochfilm.se/wp-content/uploads/2017/03/inception2.jpg",
+        title: "Inception",
+        description: "A very meta movie about dreams within dreams. And then there was a whole bunch of shooting, not really sure how that fit into the picture.",
+        actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/en/b/b6/American_Beauty_poster.jpg",
+        title: "American Beauty",
+        description: "A sexually frustrated suburban father has a mid-life crisis after becoming infatuated with his daughter's best friend.",
+        actors: ["Kevin Spacey", "Annette Benning", "Thora Birch"]
+    },
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA4MDU0NTUyN15BMl5BanBnXkFtZTgwMzQxMzY4MjE@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+        title: "Fury",
+        description: "A grizzled tank commander makes tough decisions as he and his crew fight their way across Germany in April, 1945.",
+        actors: ["Brad Pitt", "Shia LaBeouf", "Logan Lerman"]
+    },
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/I/51797H3P35L._SY445_.jpg",
+        title: "Happy Gilmore",
+        description: "A rejected hockey player puts his skills to the golf course to save his grandmother's house.",
+        actors: ["Adam Sandler", "Christopher McDonald", "Julie Bowen"]
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+        title: "Avatar",
+        description: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        actors: ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver"]
+    }
+]
